88b3d65838a8d785f1515b3370d9b0ca2c5194fc
Árpás Péter <arpaspeter0@gmail.com>
2022-09-15 08:55:01 +0200

    ToStringboi

--- src/hu/petrik/helloworld_rk/Main.java
+++ src/hu/petrik/helloworld_rk/Main.java
@@ -1,8 +1,14 @@
 package hu.petrik.helloworld_rk;
 
+import hu.petrik.pontoop.pont.Point;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Point p1 = new Point();
+        Point p2 = new Point(0 , 0);
+        Point p3 = new Point();
+
+        System.out.println(p1);
     }
 }
